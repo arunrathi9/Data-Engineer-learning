71a70a73ef513f019c1681b2807db3b7a624ae8f
ArunRathi <arunrathi201@gmail.com>
2023-04-13 10:20:52 +0530

    Update class-code.sh

--- Edureka Spark and Scala Certification/In-class work/class-code.sh
+++ Edureka Spark and Scala Certification/In-class work/class-code.sh
@@ -1,1 +1,7 @@
+#Module 4 Deep Dive into Apache spark framework
 
+# Create a folder layout for hello world program using spark and scala (for folder layout, check notes)
+sbt package # to create the jar file
+spark-submit --class "Hello" --master yarn /home/arunrathit38edu/Project/target/scala-2.11/hello-world_2.11-1.0.jar
+
+vi metastore.db.script #to check last value of the selected column on w/c we are doing the incremental import
