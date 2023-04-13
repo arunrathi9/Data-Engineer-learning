83050b631e829e7895bad104fe429cb227c285ad
ArunRathi <arunrathi201@gmail.com>
2023-04-13 11:04:34 +0530

    Update class-code.sh

--- Edureka Spark and Scala Certification/In-class work/class-code.sh
+++ Edureka Spark and Scala Certification/In-class work/class-code.sh
@@ -5,3 +5,8 @@ sbt package # to create the jar file
 spark-submit --class "Hello" --master yarn /home/arunrathit38edu/Project/target/scala-2.11/hello-world_2.11-1.0.jar
 
 vi metastore.db.script #to check last value of the selected column on w/c we are doing the incremental import
+
+# Module 5 - Playing with RDD dataframes
+# all code is in scala
+a.glom.collect() # it will show the data partition wise
+a.getNumPartitions # to get the number of partitions
